9c66c5a8605706851212b419ef539ad929b7e9ea
Vladimir Yurlov <yurlov43@gmail.com>
2022-11-06 12:11:41 +0300

    Welcome activity the layout is finished

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber;
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.tag("LifeCycles");
-        Timber.d("Activity Created");
+        Timber.tag("MainActivity").d("Activity Created");
     }
 }
