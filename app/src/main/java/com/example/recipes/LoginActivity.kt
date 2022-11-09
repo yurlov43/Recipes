417f80bc016741d0aef4aa588060aeeb4774c289
Vladimir Yurlov <yurlov43@gmail.com>
2022-11-09 10:54:24 +0300

    The processing of fields and buttons has been done

--- app/src/main/java/com/example/recipes/LoginActivity.kt
+++ app/src/main/java/com/example/recipes/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.example.recipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignInActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
